08c6af33f67404820182d4d755a9811832548603
Samuel Venzi <samuel.venzi@me.com>
2017-10-03 18:00:39 -0300

    Change in brightness depth is ready

--- proj1.cpp
+++ proj1.cpp
@@ -19,9 +19,10 @@ int main(int argc, char const *argv[])
 		std::cout << "Could not open image." << std::endl;
 		return -1;
 	}
-
+	cv::imshow("Source", src_img);
+	cv::waitKey(10);
 	//changeRedBlue(src_img);
-	im_chscaledepth(src_img, 1, 1);
+	im_chscaledepth(src_img, 3, 1);
 
 	return 0;
 }
@@ -47,21 +48,25 @@ cv::Mat changeRedBlue(cv::Mat src_img){
 
 cv::Mat im_chscaledepth(cv::Mat src_img, int depth, float scale){
 	cv::Mat proc_img, resized_img;
-	int depth_range;
-	depth_range = 256/pow(2,depth);
-	std::cout << depth_range << std::endl;
+	int depth_range, n_levels;
+
+	n_levels = pow(2,depth);
+	depth_range = 256/n_levels;
+	std::cout << n_levels << std::endl;
+	
 	proc_img = src_img;
+
 	for (int i = 0; i < src_img.cols; ++i)
 	{
 		for (int j = 0; j < src_img.rows; ++j)
 		{	
-			int level;
-			level = src_img.at<uchar>(j,i)/depth_range;
-			proc_img.at<uchar>(j,i) = level*(255);
-			//level = src_img.at<cv::Vec3b>(j,i)[1]/depth_range;
-			//proc_img.at<cv::Vec3b>(j,i)[1] = level*255;
-			//level = src_img.at<cv::Vec3b>(j,i)[2]/depth_range;
-			//proc_img.at<cv::Vec3b>(j,i)[2] = level*255;
+			for (int k = 0; k < 3; ++k)
+			{
+				int level;
+				std::cout << level << std::endl;
+				level = proc_img.at<cv::Vec3b>(j,i)[k]/depth_range;
+				proc_img.at<cv::Vec3b>(j,i)[k] = level*(255/(n_levels-1));
+			}
 		}
 	}
 
