ce5feba9c13f03c76d7ca1796d7b8126d6230b1f
Samuel Venzi <samuel.venzi@me.com>
2017-10-03 16:34:45 -0300

    Developing change in brightness depth

--- proj1.cpp
+++ proj1.cpp
@@ -2,8 +2,10 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <math.h> 
 
 cv::Mat changeRedBlue(cv::Mat src_img);
+cv::Mat im_chscaledepth(cv::Mat src_img, int depth, float scale);
 
 int main(int argc, char const *argv[])
 {
@@ -18,8 +20,8 @@ int main(int argc, char const *argv[])
 		return -1;
 	}
 
-	changeRedBlue(src_img);
-
+	//changeRedBlue(src_img);
+	im_chscaledepth(src_img, 1, 1);
 
 	return 0;
 }
@@ -40,4 +42,32 @@ cv::Mat changeRedBlue(cv::Mat src_img){
 	cv::waitKey();
 
 	return proc_img;
+}
+
+
+cv::Mat im_chscaledepth(cv::Mat src_img, int depth, float scale){
+	cv::Mat proc_img, resized_img;
+	int depth_range;
+	depth_range = 256/pow(2,depth);
+	std::cout << depth_range << std::endl;
+	proc_img = src_img;
+	for (int i = 0; i < src_img.cols; ++i)
+	{
+		for (int j = 0; j < src_img.rows; ++j)
+		{	
+			int level;
+			level = src_img.at<uchar>(j,i)/depth_range;
+			proc_img.at<uchar>(j,i) = level*(255);
+			//level = src_img.at<cv::Vec3b>(j,i)[1]/depth_range;
+			//proc_img.at<cv::Vec3b>(j,i)[1] = level*255;
+			//level = src_img.at<cv::Vec3b>(j,i)[2]/depth_range;
+			//proc_img.at<cv::Vec3b>(j,i)[2] = level*255;
+		}
+	}
+
+	cv::imshow("Brightness depth", proc_img);
+	cv::waitKey();
+
+	return proc_img;
+
 }
