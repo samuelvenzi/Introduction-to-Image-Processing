491bf7cfbbd491dc29a6cc903afd2c74ef72b04e
Samuel Venzi <samuel.venzi@me.com>
2017-10-03 14:32:53 -0300

    Initial files

---
+++ proj1.cpp
@@ -0,0 +1,11 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
