ce62db5922b7a02e9b1c1032f39f649ba0531b5e
Samuel Venzi <samuel.venzi@me.com>
2017-10-06 20:53:31 -0300

    Question 2 started. Laplacian filter is working

---
+++ question2.cpp
@@ -0,0 +1,41 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <math.h> 
+
+int main(int argc, char const *argv[])
+{
+	cv::Mat src_img, proc_img;
+
+	src_img = cv::imread(argv[1]);
+	if (!src_img.data)
+	{
+		std::cout << "Could not open image." << std::endl;
+		return -1;
+	}
+
+	cv::Mat kernel;
+	
+	kernel = (cv::Mat_<float>(3,3) << 
+        -1,  -1, -1,
+        -1, 8, -1,
+        -1,  -1, -1);
+
+	printf("Debug\n");
+	cv::imshow("Source", src_img);
+	cv::waitKey(10);
+	cv::imshow("Kernel", kernel);
+	cv::waitKey(10);
+
+
+	//cv::Laplacian(src_img, proc_img, 8);
+
+	cv::filter2D(src_img, proc_img, -1, kernel);
+
+	cv::imshow("Laplacian", proc_img);
+	cv::waitKey();
+
+
+	return 0;
+}
