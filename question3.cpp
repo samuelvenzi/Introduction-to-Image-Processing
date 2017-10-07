5a861497fc922cd0cb2cf6910369de36c66b3a65
Samuel Venzi <samuel.venzi@me.com>
2017-10-07 12:14:58 -0300

    Question 3 sorta started

---
+++ question3.cpp
@@ -0,0 +1,12 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <math.h> 
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	
+	return 0;
+}
