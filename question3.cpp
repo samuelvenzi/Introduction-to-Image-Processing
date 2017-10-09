df528bd5729805f3e47ccbe0fae4e645be6d60c2
Samuel Venzi <samuel.venzi@me.com>
2017-10-08 21:11:11 -0300

    Question 3 is crashing

--- question3.cpp
+++ question3.cpp
@@ -6,7 +6,32 @@
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	cv::Mat src_img, freq_img;
+
+	src_img = cv::imread(argv[1]);
+	if (!src_img.data)
+	{
+		std::cout << "Could not open image." << std::endl;
+		return -1;
+	}
+	cv::imshow("Source", src_img);
+	cv::waitKey(10);
+
+	// cv::Mat padded;                            //expand input image to optimal size
+ //    int m = cv::getOptimalDFTSize(src_img.rows);
+ //    int n = cv::getOptimalDFTSize(src_img.cols); // on the border add zero values
+ //    copyMakeBorder(src_img, padded, 0, m - src_img.rows, 0, n - src_img.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+
+
+	// printf("Debug\n");
+ //    cv::Mat planes[] = {cv::Mat_<float>(padded), cv::Mat::zeros(padded.size(), CV_32F)};
+	
+ //    cv::merge(planes, 2, freq_img);         // Add to the expanded another plane with zeros
+
+    cv::dft(src_img, freq_img);            // this way the result may fit in the source matrix
+
+
+
 	
 	return 0;
 }
