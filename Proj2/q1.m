70b76208d1756e6cf65758f4de8739106c107be9
Samuel Venzi <samuel.venzi@me.com>
2017-11-14 11:29:02 -0200

    Q1 of project 2 almost finished

---
+++ Proj2/q1.m
@@ -0,0 +1,20 @@
+src = imread('cookies.tif');
+
+bin_img = im2bw(src, 0.4)
+figure, imshow(bin_img);
+SE = strel('disk',60, 0)
+IM = imerode(bin_img,SE)
+
+figure, imshow(IM);
+hold on;
+SE = strel('disk',70, 0)
+IM2 = imdilate(IM, SE)
+
+figure, imshow(IM2);
+
+cookie = bitand(IM2,bin_img)
+
+figure, imshow(cookie);
+
+result = immultiply(cookie,src)
+figure, imshow(result);
